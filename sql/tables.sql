d490afc79d750892b9305a00c4358c043a90d80c
lzlaG <p0chta1234prosto@yandex.ru>
2024-04-19 12:59:39 +0500

    правка таблиц

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,10 +1,9 @@
 CREATE TABLE Клиенты (
     client_id INT PRIMARY KEY NOT NULL,
     full_name VARCHAR(100),
-    adres  VARCHAR(100),
+    adress  VARCHAR(100),
     phone_number VARCHAR(10)
 );
-
 CREATE TABLE Поставщики (
     supplier_id INT PRIMARY KEY NOT NULL,
     naming VARCHAR(100)
@@ -13,36 +12,34 @@ CREATE TABLE Поставщики (
 CREATE TABLE Продавцы (
     customer_id INT PRIMARY KEY NOT NULL,
     full_name VARCHAR(100),
-    salary INT,
-    num_of_complited_orders INT,
-    num_of_uncomplited_orders INT
+    salary INT
+    -- мб num_of_complited_orders INT,--
+    -- мб num_of_uncomplited_orders INT--
 );
 
 CREATE TABLE Блюда (
     dishe_id INT PRIMARY KEY NOT NULL,
     naming VARCHAR(50),
-    calory INT,
-    sostav VARCHAR(100)
+    calory INT
 );
 
 CREATE TABLE Ингридиенты (
     ingridient_id INT PRIMARY KEY NOT NULL,
     naming VARCHAR(50),
-    price INT,
-    dishe INT REFERENCES Блюда(dishe_id)
+    price INT
+);
+
+CREATE TABLE Состав_Блюда(
+    dishe_id INT REFERENCES Блюда(dishe_id),
+    ingridient_id INT REFERENCES Ингридиенты(ingridient_id)
 );
 
 CREATE TABLE Поставки (
     supply_id INT PRIMARY KEY NOT NULL,
     supplier INT REFERENCES Поставщики(supplier_id),
-    ingridient INT REFERENCES Ингридиенты(ingridient_id)
-);
-
-CREATE TABLE Использование_ингридиентов (
-    ingridient_usage_id INT PRIMARY KEY NOT NULL,
     ingridient INT REFERENCES Ингридиенты(ingridient_id),
-    kolvo INT,
-    num_of_week INT
+    amount_of_ingridients INT,
+    date_of_suply DATE
 );
 
 CREATE TABLE Штрафы (
@@ -60,10 +57,14 @@ CREATE TABLE Заказы(
     order_id INT PRIMARY KEY NOT NULL,
     client INT REFERENCES Клиенты(client_id),
     customer INT REFERENCES Продавцы (customer_id),
-    usage INT REFERENCES Использование_ингридиентов (ingridient_usage_id),
     status_of_order INT REFERENCES Статус_заказа(status_id),
     date_of_order date,
     sum_of_order INT
+);
+
+CREATE TABLE Состав_заказа(
+    order_id INT REFERENCES Заказы(order_id),
+    dishe_id INT REFERENCES Блюда(dishe_id)
 );
 
 
